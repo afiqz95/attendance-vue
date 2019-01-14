0234abb39c1631546035f081c181f10a9b095512
Muhammad Afiq Ahmad Zailani <afiqzailani0@gmail.com>
2019-01-14 15:26:53 +0800

    Clean up

---
+++ ClientApp/components/management/adduser.ts
@@ -0,0 +1,10 @@
+import Vue from "vue";
+import { Component } from "vue-property-decorator";
+
+export default Vue.extend({
+  methods: {
+    insertNewUser() {
+      //INSERT TO API
+    }
+  }
+});
